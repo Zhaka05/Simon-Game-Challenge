1d6caf0405162d8e7d90b7b1a291b549010f0baa
Zhaka <shingis.zhansen@gmail.com>
2026-01-06 12:48:06 -0600

    add basic game logic till step 3, connect jquery

--- games.js
+++ games.js
@@ -1,1 +1,12 @@
-alert("Hello JS!")
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100);
+    
+    let sound = new Audio("sounds/" + randomChosenColour + ".mp3");
+    sound.play()
+}
+nextSequence();
